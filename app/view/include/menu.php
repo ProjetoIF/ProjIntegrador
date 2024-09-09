376a757adb45d4c936cf2a0eb209cb3c011526b6
L0K2 <rafazsmine517@gmail.com>
2024-09-09 15:45:47 -0300

    Atualização estilização

--- app/view/include/menu.php
+++ app/view/include/menu.php
@@ -6,40 +6,6 @@ $nome = "(Sessão expirada)";
 if(isset($_SESSION[SESSAO_USUARIO_NOME]))
     $nome = $_SESSION[SESSAO_USUARIO_NOME];
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?= HOME_PAGE ?>">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cadastros</a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>">Usuários</a></li>
-                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/DisciplinaController.php?action=list' ?>">Disciplinas</a></li>
-                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/IngredienteController.php?action=list' ?>">Ingredientes</a></li>
-                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/TurmaController.php?action=list' ?>">Turmas</a></li>
-                        <li><a class="dropdown-item" href="<?= BASEURL . '/controller/RequisicoesController.php?action=list' ?>">Requisicoes</a></li>
-                    </ul>
-                </li>
-
-                <li class="nav-item active">
-                    <a class="nav-link" href="<?= LOGOUT_PAGE ?>">Sair</a>
-                </li>
-            </ul>
-
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item active">
-                    <?= $nome ?>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
 <div class="sidebar">
     <div class="user-info container d-flex">
         <img src="caminho_para_imagem_do_usuario" alt="User Image" class="user-image">
@@ -50,14 +16,15 @@ if(isset($_SESSION[SESSAO_USUARIO_NOME]))
         </div>
     </div>
     <ul class="sidebar-menu">
-        <li><a href="#"><i class="fas fa-lock"></i>Criar requisição</a></li>
-        <li><a href="<?= BASEURL . '/controller/RequisicoesController.php?action=list' ?>""><i class="fas fa-lock"></i>Requisições</a></li>
-        <li><a href="#"> <i class="fas fa-lock"></i>Disciplinas</a></li>
-        <li><a href="#"> <i class="fas fa-lock"></i>Turmas</a></li>
-        <li><a href="#"> <i class="fas fa-lock"></i>Ingredientes</a></li>
-        <li><a href="#"><i class="fas fa-lock"></i>Relatórios</a></li>
-        <li><a href="#"><i class="fas fa-lock"></i>Adicionar usuários</a></li>
-        <li><a href="<?= LOGOUT_PAGE ?>"><i class="fas fa-lock"></i>Sair</a></li>
+        <li><a href="<?= HOME_PAGE ?>"><i class="fa-solid fa-house"></i>Home</a></li>
+        <li><a href="<?= BASEURL . '/controller/UsuarioController.php?action=list' ?>"><i class="fa-solid fa-users"></i>Usuários</a></li>
+        <li><a href="<?= BASEURL . '/controller/RequisicoesController.php?action=list' ?>""><i class="fa-solid fa-clipboard-list"></i>Requisições</a></li>
+        <li><a href="<?= BASEURL . '/controller/DisciplinaController.php?action=list' ?>"> <i class="fa-solid fa-book"></i>Disciplinas</a></li>
+        <li><a href="<?= BASEURL . '/controller/TurmaController.php?action=list' ?>"> <i class="fa-solid fa-graduation-cap"></i></i>Turmas</a></li>
+        <li><a href="<?= BASEURL . '/controller/IngredienteController.php?action=list' ?>"> <i class="fa-solid fa-utensils"></i>Ingredientes</a></li>
+        <li><a href="#"><i class="fa-solid fa-chart-simple"></i>Relatórios</a></li>
+        <li><a href="#"><i class="fa-solid fa-user-plus"></i>Adicionar usuários</a></li>
+        <li><a href="<?= LOGOUT_PAGE ?>"><i class="fa-solid fa-right-from-bracket"></i>Sair</a></li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cadastros</a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
--- app/view/usuario/list.php
+++ app/view/usuario/list.php
@@ -6,63 +6,74 @@ require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
 ?>
 
-<h3 class="text-center">Usuários</h3>
-
-<div class="container">
-    <div class="row">
-        <div class="col-3">
-            <a class="btn btn-success" 
-                href="<?= BASEURL ?>/controller/UsuarioController.php?action=create">
-                Inserir</a>
-        </div>
-
-        <div class="col-9">
-            <?php require_once(__DIR__ . "/../include/msg.php"); ?>
+    <div class="container nav-pages">
+        <div class="col">
+            <div class="row">
+                <h3 class="font-weight-bold poppins-extrabold">Usuários</h3>
+                <div class="align-items-end">
+                    <button class="btn">Home</button>
+                </div>
         </div>
     </div>
 
-    <div class="row" style="margin-top: 10px;">
-        <div class="col-12">
-            <table id="tabUsuarios" class='table table-striped table-bordered'>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Login</th>
-                        <th>Papel</th>
-                        <th>Telefone</th>
-                        <th>Email</th>
-                        <th>Status</th>
-                        <th>Alterar</th>
-                        <th>Excluir</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($dados['lista'] as $usu): ?>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <a class="btn btn-success"
+                    href="<?= BASEURL ?>/controller/UsuarioController.php?action=create">
+                    Inserir</a>
+            </div>
+
+            <div class="col-9">
+                <?php require_once(__DIR__ . "/../include/msg.php"); ?>
+            </div>
+        </div>
+
+        <div class="row" style="margin-top: 10px;">
+            <div class="col-12">
+                <table id="tabUsuarios" class='table table-striped table-bordered'>
+                    <thead>
                         <tr>
-                            <td><?php echo $usu->getId(); ?></td>
-                            <td><?= $usu->getNome(); ?></td>
-                            <td><?= $usu->getLogin(); ?></td>
-                            <td><?= $usu->getPapel(); ?></td>
-                            <td><?= $usu->getTelefone(); ?></td>
-                            <td><?= $usu->getEmail(); ?></td>
-                            <td><?= $usu->isActive(); ?></td>
-                            <td><a class="btn btn-primary"
-                                href="<?= BASEURL ?>/controller/UsuarioController.php?action=edit&id=<?= $usu->getId() ?>">
-                                Alterar</a> 
-                            </td>
-                            <td><a class="btn btn-danger" 
-                                onclick="return confirm('Confirma a exclusão do usuário?');"
-                                href="<?= BASEURL ?>/controller/UsuarioController.php?action=delete&id=<?= $usu->getId() ?>">
-                                Excluir</a> 
-                            </td>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Login</th>
+                            <th>Papel</th>
+                            <th>Telefone</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Alterar</th>
+                            <th>Excluir</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach($dados['lista'] as $usu): ?>
+                            <tr>
+                                <td><?php echo $usu->getId(); ?></td>
+                                <td><?= $usu->getNome(); ?></td>
+                                <td><?= $usu->getLogin(); ?></td>
+                                <td><?= $usu->getPapel(); ?></td>
+                                <td><?= $usu->getTelefone(); ?></td>
+                                <td><?= $usu->getEmail(); ?></td>
+                                <td><?= $usu->isActive(); ?></td>
+                                <td><a class="btn btn-primary"
+                                    href="<?= BASEURL ?>/controller/UsuarioController.php?action=edit&id=<?= $usu->getId() ?>">
+                                        <i class="fa-regular fa-pen-to-square"></i> Alterar</a>
+                                </td>
+                                <td><a class="btn btn-danger"
+                                    onclick="return confirm('Confirma a exclusão do usuário?');"
+                                    href="<?= BASEURL ?>/controller/UsuarioController.php?action=delete&id=<?= $usu->getId() ?>">
+                                        <i class="fa-solid fa-trash-can"></i> Excluir</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
+
 
 <?php  
 require_once(__DIR__ . "/../include/footer.php");
